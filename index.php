aa7d395782fdfe9c388b1bedd936463d91e0f1b1
petro-p <petropobyy@gmail.com>
2019-07-10 12:32:13 +0200

    actualizacion

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-$peña = [
+$personas = [
     'Julia',
     'Jorge',
     'Jose',
@@ -18,73 +18,16 @@ $peña = [
     'Luis',
     'Toni'
 ];
-$julia = 'Julia';
-$jorge = 'Jorge';
-$jose = 'Jose';
-$mateo = 'Mateo';
-$jaume = 'Jaume';
-$petro = 'Petro';
-$alejandro = 'Alejandro';
-$fran = 'Fran';
-$adri = 'Adri';
-$lolo = 'Lolo';
-$cristian = 'Cristian';
-$alexis = 'Alexis';
-$david = 'David';
-$jordi = 'Jordi';
-$luis = 'Luis';
-$toni = 'Toni';
 
+$personaUno = array_rand($personas, 2);
+$personaDos = array_rand($personas, 2);
 
+echo '<h1 style="text-align:center;">'. $personas[$personaUno[0]]. ' · ' .$personas[$personaDos[0]] .'</h1>';
 
-echo($random = rand(0,15));
-
-if($random = 0){
-    echo('Julia');
+if ( $nombre1 = array_search ($personas[$personaUno[0]], $personas ) ) {
+    unset($personas[$nombre1]);
 }
-else if($random = 1){
-    echo('Jorge');
+if ( $nombre2 = array_search ($personas[$personaDos[0]], $personas ) ) {
+    unset($personas[$nombre2]);
 }
-else if($random = 2){
-    echo('Jose');
-}
-else if($random = 3){
-    echo('Mateo');
-}
-else if($random = 4){
-    echo('Jaume');
-}
-else if($random = 5){
-    echo('Petro');
-}
-else if($random = 6){
-    echo('Alejandro');
-}
-else if($random = 7){
-    echo('Fran');
-}
-else if($random = 8){
-    echo('Adri');
-}
-else if($random = 9){
-    echo('Lolo');
-}
-else if($random = 10){
-    echo('Cristian');
-}
-else if($random = 11){
-    echo('Alexis');
-}
-else if($random = 12){
-    echo('David');
-}
-
-
-
-
-
-
-
-
-
-// $julia,$jorge,$jose,$mateo,$jaume,$petro,$alejandro,$fran,$adri,$lolo,$cristian,$alexis,$david,$jordi,$luis,$toni
+// echo($random = rand(0,15) . '. ');
